c0af62c1b96649926352e30a644621ac0aa69270
tharunoptimus <tharunoptimus@outlook.com>
2023-05-28 17:09:37 +0530

    Created bard answering function

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ from flask import Flask, request, render_template, send_file
 from flask_cors import CORS
 import requests
 from bardapi import Bard
+
+bard_token = ""
+
+def get_questions(captions, question):
+    bard = Bard(token=bard_token, timeout=30)
+    prompt = captions + "\n\Answer in a single sentence. Don't add filler words. Just be precise.\n\n" + question
+    response = bard.get_answer(prompt)['content']
+    return response
+    
+
